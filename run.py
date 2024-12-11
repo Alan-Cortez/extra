ad66557502071d13565591dca49f88d1b39d5fa5
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-12-11 16:08:51 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-from flask import Flask
-from apps.controllers.usuarios_controller import usuarios_bp
-
-def create_app():
-    app = Flask(__name__)
-    
-    # Registrar blueprints
-    app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
-
-    return app
+from apps import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-
