968697381adc87eb11f5e702a9ea6b2e34405723
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-12-11 16:11:10 -0600

    Update usuario.py

--- apps/models/usuario.py
+++ apps/models/usuario.py
@@ -1,29 +1,52 @@
 import mysql.connector
+from apps.config import Config
 
 class Usuario:
     @staticmethod
-    def obtener_todos():
-        con = mysql.connector.connect(
-            host="185.232.14.52",
-            database="u760464709_tst_sep",
-            user="u760464709_tst_sep_usr",
-            password="dJ0CIAFF="
+    def obtener_conexion():
+        return mysql.connector.connect(
+            host=Config.MYSQL_HOST,
+            database=Config.MYSQL_DATABASE,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD
         )
-        cursor = con.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM usuarios")
-        usuarios = cursor.fetchall()
-        con.close()
-        return usuarios
 
     @staticmethod
-    def crear(nombre, email):
-        con = mysql.connector.connect(
-            host="185.232.14.52",
-            database="u760464709_tst_sep",
-            user="u760464709_tst_sep_usr",
-            password="dJ0CIAFF="
-        )
+    def crear_usuario(usuario, contrasena):
+        con = Usuario.obtener_conexion()
         cursor = con.cursor()
-        cursor.execute("INSERT INTO usuarios (nombre, email) VALUES (%s, %s)", (nombre, email))
+        sql = "INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)"
+        cursor.execute(sql, (usuario, contrasena))
         con.commit()
+        cursor.close()
+        con.close()
+
+    @staticmethod
+    def obtener_usuarios():
+        con = Usuario.obtener_conexion()
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM tst0_usuarios ORDER BY Id_Usuario DESC")
+        registros = cursor.fetchall()
+        cursor.close()
+        con.close()
+        return registros
+
+    @staticmethod
+    def actualizar_usuario(id_usuario, usuario, contrasena):
+        con = Usuario.obtener_conexion()
+        cursor = con.cursor()
+        sql = "UPDATE tst0_usuarios SET Nombre_Usuario=%s, Contrasena=%s WHERE Id_Usuario=%s"
+        cursor.execute(sql, (usuario, contrasena, id_usuario))
+        con.commit()
+        cursor.close()
+        con.close()
+
+    @staticmethod
+    def eliminar_usuario(id_usuario):
+        con = Usuario.obtener_conexion()
+        cursor = con.cursor()
+        sql = "DELETE FROM tst0_usuarios WHERE Id_Usuario=%s"
+        cursor.execute(sql, (id_usuario,))
+        con.commit()
+        cursor.close()
         con.close()
